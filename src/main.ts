d7e703fc1051ae82f5dbd6ab74022c6ce86d2fa0
kuhlin <gidiaconu@live.com>
2023-08-18 15:33:23 +0200

    Using of and for from rxjs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'zone.js/dist/zone';
 import { Component, VERSION, Version } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -16,6 +17,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 })
 export class App {
   name = 'Angular: ' + VERSION.major;
+
+  ngOnInit() {
+    of(2, 4, 6, 8).subscribe((item) => console.log(item));
+
+    from([20, 15, 10, 5]).subscribe({
+      next: (item) => console.log(`resulting item ..` + item),
+      error: (err) => console.log(`error ocurred ` + err),
+      complete: () => console.log('compete'),
+    });
+  }
 }
 
 bootstrapApplication(App);
